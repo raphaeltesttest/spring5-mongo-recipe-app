5c6eb9c6650e79e7f1fc12f3a54eb3b2e8273121
marianos <marianos8125@gmail.com>
2021-02-02 09:38:26 -0300

    correcting application defects under MongoDB

--- src/main/java/guru/springframework/services/IngredientServiceImpl.java
+++ src/main/java/guru/springframework/services/IngredientServiceImpl.java
@@ -55,6 +55,9 @@ public class IngredientServiceImpl implements IngredientService {
             log.error("Ingredient id not found: " + ingredientId);
         }
 
+        IngredientCommand ingredientCommand = ingredientCommandOptional.get();
+        ingredientCommand.setRecipeId(recipeId);
+
         return ingredientCommandOptional.get();
     }
 
@@ -108,11 +111,7 @@ public class IngredientServiceImpl implements IngredientService {
             }
 
             //to do check for fail
-//            return ingredientToIngredientCommand.convert(savedIngredientOptional.get());
-            IngredientCommand ingredientCommandSaved = ingredientToIngredientCommand.convert(savedIngredientOptional.get());
-            ingredientCommandSaved.setRecipeId(recipe.getId());
-
-            return ingredientCommandSaved;
+            return ingredientToIngredientCommand.convert(savedIngredientOptional.get());
         }
 
     }
